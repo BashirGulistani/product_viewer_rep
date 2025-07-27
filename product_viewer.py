2a0f0fc80c65dab1d77f55cd3c24a7f1734eec9f
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2025-07-27 19:57:00 -0400

    Update product_viewer.py

--- product_viewer.py
+++ product_viewer.py
@@ -23,7 +23,7 @@ logo_url = "https://raw.githubusercontent.com/BashirGulistani/product_viewer_rep
 html_header = f'''
 <div style="
     position: fixed;
-    top: 0.2;
+    top: 0;
     left: 0;
     width: 100%;
     background-color: #0E3B53;
