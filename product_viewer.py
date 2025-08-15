e0e429d708828e35f7973553a20003a35fcb565a
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2025-08-15 19:12:01 -0400

    Update product_viewer.py

--- product_viewer.py
+++ product_viewer.py
@@ -400,6 +400,7 @@ if st.session_state.favorites:
             col1, col2 = st.columns([3, 1])
             with col1:
                 st.write(clean_product_name(product.get("productName")))
+                st.write(f"------Item#{product.get("productId"}))
             #with col2:
             #    st.button("Remove", key=f"remove_{product['productId']}", on_click=remove_from_favorites, args=[str(product['productId'])])
         
